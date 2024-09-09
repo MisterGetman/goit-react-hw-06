c809acf52ac93b4a4ad9a4cb0bb18c8572ce753e
MisterGetman <99386291+MisterGetman@users.noreply.github.com>
2024-09-09 16:06:19 +0300

    Upd.

--- src/main.jsx
+++ src/main.jsx
@@ -7,12 +7,15 @@ import "./index.css";
 
 import App from "./App.jsx";
 import { Provider } from "react-redux";
-import { store } from "./redux/store";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistor, store } from "./redux/store";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
